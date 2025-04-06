1e9c3910b8d621bd313280258c3be1b09869dc75
Diogo Bandeira Assunção <132160042+Diogoassun@users.noreply.github.com>
2025-04-06 14:49:16 -0300

    Create Extremamente_Basico.c

---
+++ Nível Iniciante/Extremamente Básico/Solution in C/Extremamente_Basico.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num1, num2, soma;
+
+   scanf("%d", &num1);
+
+   scanf("%d", &num2);
+
+   soma = num1 + num2;
+
+   printf("X = %d\n", soma);
+ 
+    return 0;
+}
